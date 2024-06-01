582aa154b765c580a030ec738491be700c125d97
Gabriel Aizman <gabaiz88@gmail.com>
2024-06-01 16:30:18 +0200

    limpiando codigo

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@ var language = {
     second_job: "Bank Teller",
     third_job: "Credit Risk Analyst",
     fourth_job: "Head Teller",
-    fifth_job: "Teller",
+    fifth_job: "Teller",<
   },
   es: {
     welcome: "UN POCO ACERCA DE MI..",
@@ -51,8 +51,6 @@ var language = {
 
 /* // Check if a hash value exists in the URL
 if (window.location.hash) {
-  // Set the content of the webpage
-  // depending on the hash value
   if (window.location.hash == "#es") {
     about_me.textContent = language.es.welcome;
     about_me_description.textContent = language.es.decription;
@@ -76,69 +74,73 @@ if (window.location.hash) {
   }
 } */
 
-
 //chatbot
 const responses = {
-  "vives": "Vivo en Valencia. C.P. 46024, España",
-  "trabajo": "Mi último trabajo fue como asesor comercial, en una Universidad en Valencia. Allí he autogestionado una cartera de prospectos, promoviendo formaciones académicas a través de contactos telefónicos y WhatsApp. He asesorado a los alumnos en su inscripción, destacando beneficios y resolviendo dudas para asegurar el cierre de matriculaciones. También he realizado seguimiento y gestión administrativa, manejando situaciones como retrasos en pagos para garantizar una experiencia educativa satisfactoria.",
-  "trabajo anterior": "En Argentina trabajé más de 12 años en el sector bancario, finalizando como tesorero / Lider de equipo en Banco Credicoop. Brindé asesoramiento financiero estratégico, gestioné la tesorería y mantuve relaciones sólidas con clientes y proveedores, asegurando un servicio al cliente excelente y resolviendo problemas eficazmente.",
-  "tiempo libre": "En mis tiempos libres, me gusta ver peliculas y series. Soy aficionado por las películas de fantasía como Harry Potter o El señor de los anillos. También me gusta jugar al padel o salir a caminar.",
-  "herramientas": "Manejo lenguajes de programación como JavaScript, HTML, CSS y Java, junto con frameworks y librerías como React, Bootstrap, Node.js y Tailwind CSS. También utilizo herramientas como Git para control de versiones, Figma para diseño y Firebase para desarrollo backend. Además, aplico metodologías ágiles para gestionar y optimizar los proyectos de manera eficiente.",
-  "certificaciones": "He completado cursos y certificaciones en Metodologías Ágiles (Scrum) en Udemy (2024), Desarrollo Frontend con React en Coderhouse (2022-2023), Software Testing en Udemy (2023), y poseo un nivel de inglés B2, estudiando actualmente en ABA English (2024).",
-  "futuro": "A futuro me veo habiendo avanzado significativamente en mi carrera, asumiendo roles de mayor responsabilidad y liderazgo. Espero haber adquirido nuevas competencias técnicas y de gestión, y estar contribuyendo de manera estratégica a los proyectos del equipo. También me gustaría estar involucrado en iniciativas que impulsen la innovación dentro de mi campo.",
+  vives: "Vivo en Valencia. C.P. 46024, España",
+  trabajo:
+    "Mi último trabajo fue como asesor comercial, en una Universidad en Valencia. Allí he autogestionado una cartera de prospectos, promoviendo formaciones académicas a través de contactos telefónicos y WhatsApp. He asesorado a los alumnos en su inscripción, destacando beneficios y resolviendo dudas para asegurar el cierre de matriculaciones. También he realizado seguimiento y gestión administrativa, manejando situaciones como retrasos en pagos para garantizar una experiencia educativa satisfactoria.",
+  "trabajo anterior":
+    "En Argentina trabajé más de 12 años en el sector bancario, finalizando como tesorero / Lider de equipo en Banco Credicoop. Brindé asesoramiento financiero estratégico, gestioné la tesorería y mantuve relaciones sólidas con clientes y proveedores, asegurando un servicio al cliente excelente y resolviendo problemas eficazmente.",
+  "tiempo libre":
+    "En mis tiempos libres, me gusta ver peliculas y series. Soy aficionado por las películas de fantasía como Harry Potter o El señor de los anillos. También me gusta jugar al padel o salir a caminar.",
+  herramientas:
+    "Manejo lenguajes de programación como JavaScript, HTML, CSS y Java, junto con frameworks y librerías como React, Bootstrap, Node.js y Tailwind CSS. También utilizo herramientas como Git para control de versiones, Figma para diseño y Firebase para desarrollo backend. Además, aplico metodologías ágiles para gestionar y optimizar los proyectos de manera eficiente.",
+  certificaciones:
+    "He completado cursos y certificaciones en Metodologías Ágiles (Scrum) en Udemy (2024), Desarrollo Frontend con React en Coderhouse (2022-2023), Software Testing en Udemy (2023), y poseo un nivel de inglés B2, estudiando actualmente en ABA English (2024).",
+  futuro:
+    "A futuro me veo habiendo avanzado significativamente en mi carrera, asumiendo roles de mayor responsabilidad y liderazgo. Espero haber adquirido nuevas competencias técnicas y de gestión, y estar contribuyendo de manera estratégica a los proyectos del equipo. También me gustaría estar involucrado en iniciativas que impulsen la innovación dentro de mi campo.",
 };
 
 function handleUserInput() {
-  const userSelect = document.getElementById('user-select');
+  const userSelect = document.getElementById("user-select");
   const selectedValue = userSelect.value;
   const selectedText = userSelect.options[userSelect.selectedIndex].text;
-  
+
   if (selectedValue) {
-      addUserMessage(selectedText);
-      addBotResponse(selectedValue);
-      userSelect.value = '';
+    addUserMessage(selectedText);
+    addBotResponse(selectedValue);
+    userSelect.value = "";
   } else {
-      alert('Por favor, selecciona una pregunta.');
+    alert("Por favor, selecciona una pregunta.");
   }
 }
 
 function addUserMessage(message) {
-  const chat = document.getElementById('chat');
-  const userMessageDiv = document.createElement('div');
-  userMessageDiv.className = 'message user-message';
+  const chat = document.getElementById("chat");
+  const userMessageDiv = document.createElement("div");
+  userMessageDiv.className = "message user-message";
   userMessageDiv.innerText = message;
   chat.appendChild(userMessageDiv);
 }
 
 function addBotResponse(selectedValue) {
-  const chat = document.getElementById('chat');
-  const botMessageDiv = document.createElement('div');
-  botMessageDiv.className = 'message bot-message';
+  const chat = document.getElementById("chat");
+  const botMessageDiv = document.createElement("div");
+  botMessageDiv.className = "message bot-message";
   chat.appendChild(botMessageDiv);
-  
+
   const response = responses[selectedValue];
   typeText(botMessageDiv, response);
 }
 
 function typeText(element, text, index = 0) {
   if (index < text.length) {
-      const nextChar = document.createTextNode(text.charAt(index));
-      element.appendChild(nextChar);
-      setTimeout(() => {
-          typeText(element, text, index + 1);
-          // Desplaza el contenedor del chat hacia abajo
-          scrollToBottom();
-      }, 40);
+    const nextChar = document.createTextNode(text.charAt(index));
+    element.appendChild(nextChar);
+    setTimeout(() => {
+      typeText(element, text, index + 1);
+      // Desplaza el contenedor del chat hacia abajo
+      scrollToBottom();
+    }, 40);
   }
 }
 
 function scrollToBottom() {
-  const chat = document.getElementById('chat');
+  const chat = document.getElementById("chat");
   chat.scrollTop = chat.scrollHeight;
 }
 
-
-// SKILLS
+// Skills
 const skillContent = document.querySelectorAll(".skill");
 const skillHeader = document.querySelectorAll(".skills_header");
 const skillContentArr = Array.from(skillContent);
